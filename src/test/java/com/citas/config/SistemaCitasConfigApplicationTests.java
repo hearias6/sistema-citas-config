116761bc257ce8d4773e316084f2bb754a850891
hans.arias <hearias6@misena.edu.co>
2020-05-21 14:39:10 -0500

    organizar los perfiles

---
+++ src/test/java/com/citas/config/SistemaCitasConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.citas.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaCitasConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
